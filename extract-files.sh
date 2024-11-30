fbb7917ec734f2fc9555c1dbeec3438069f95930
vherawidatama <vherawidatama@gmail.com>
2024-11-30 04:30:25 +0000

    ingres: sync blobs & extractor-files
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -19,9 +19,13 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
+        vendor/lib64/hw/audio.primary.taro-ingres.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-ingres.so" "${2}"
+            ;;
         vendor/lib64/libkaraokepal.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro-ingres.so" "${2}"
+            "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-marble.so" "${2}"
             ;;
         *)
             return 1
