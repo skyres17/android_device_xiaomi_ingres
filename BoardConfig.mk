e1de7c71a23748b276624a2e7a71e677baad8e56
ParsaAslaniYC <parsaaslani.67@gmail.com>
2025-10-21 18:10:36 +0000

    ingres: Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8450-common
+include device/xiaomi/sm8450-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+include vendor/xiaomi/ingres/BoardConfigVendor.mk
+
+DEVICE_PATH := device/xiaomi/ingres
---
+++ lineage_ingres.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from ingres device
+$(call inherit-product, device/xiaomi/ingres/device.mk)
+
+# Inherit from common lineage configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_ingres
+PRODUCT_DEVICE := ingres
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 21121210G
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
